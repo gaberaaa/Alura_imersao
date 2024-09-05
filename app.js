546b2eb432707730641e6819a333677cc612672f
Gabriel Silva <150830063+gaberaaa@users.noreply.github.com>
2024-09-05 14:46:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+let resultados = ""
+
+for(let dado of dados){
+    resultados += `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">More Information</a>
+    </div>
+`;
+
+}
+
+section.innerHTML = resultados
+}
+
---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo : "AK-47",
+        descricao : "A AK-47 é uma das armas mais poderosas e populares no jogo Rust. Sua alta taxa de disparo e dano a tornam uma escolha ideal para combates, tanto à curta quanto à média distância. É fácil de encontrar munição para ela no mapa, e a arma pode ser encontrada em diversos locais ou construída com os componentes certos. No entanto, seu recuo pode ser desafiador, e é importante dominar o controle da arma para maximizar sua eficácia. Para dominar o combate no Rust, dominar a AK-47 é essencial.",
+        link : "https://wiki.rustclash.com/item/assault-rifle#tab=loot;sort=3,1,0"
+    },
+    {
+        titulo : "MP5",
+        descricao : "A MP5 é uma submetralhadora popular no Rust, conhecida por sua alta taxa de disparo e precisão a curta distância. Ideal para combates em espaços fechados ou em situações que exigem rápida resposta, a MP5 é uma ótima opção para jogadores que preferem um estilo de jogo mais agressivo. ",
+        link : "https://wiki.rustclash.com/item/mp5a4"
+    },
+    {
+        titulo : "L96 Rifle",
+        descricao : "A L96 é um rifle de precisão de alto calibre no Rust, conhecido por sua precisão extrema e alcance letal. Ideal para eliminar alvos a longas distâncias, a L96 é uma arma de elite que exige habilidade para dominar. ",
+        link : "https://wiki.rustclash.com/item/l96-rifle"
+    }
+]
+
