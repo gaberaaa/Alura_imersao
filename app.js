d9b66c6d9cabe4b0c86a05ab89594424d65f6011
Gabriel Silva <150830063+gaberaaa@users.noreply.github.com>
2024-09-06 13:57:11 -0300

    final version

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa")
-let resultados = ""
 
-for(let dado of dados){
-    resultados += `
-    <div class="item-resultado">
-        <h2>
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nem um resultado encontrado. Você precisa digitar o nome de um item.</p>";
+        return
+
+    }
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tag = "";
+
+    for(let dado of dados){
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tag = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tag.includes(campoPesquisa)){
+
+            resultados += `
+            <div class="item-resultado">
+            <h2>
             <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href=${dado.link} target="_blank">More Information</a>
-    </div>
-`;
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">More Information</a>
+            </div>
 
-}
+            `;
+
+        }
+
+        if (!resultados){
+            resultados = "<p>Resultado nao encontrado</p>";
+        }
+    
+
+    }
 
 section.innerHTML = resultados
 }
